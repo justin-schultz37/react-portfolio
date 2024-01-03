8ee9c52065571b057f43ee4ab1d6dd0a19e94abc
birdslisten <justin.schultz37@gmail.com>
2024-01-02 17:41:36 -0800

    removed some css effects

--- Portfolio/src/pages/Project.jsx
+++ Portfolio/src/pages/Project.jsx
@@ -3,14 +3,14 @@ import data from '../utils/projects.json';
 
 const Project = () => {
     return (
-        <div>
+        <div className="card-container" style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around' }}>
             {data?.projects?.map((project) => (
-                <div key={project.id} className="card" style={{ width: '18rem' }}>
+                <div key={project.id} className="card" style={{ width: '18rem', marginBottom: '20px' }}>
                     <img src={project.src} className="card-img-top" alt={project.title} />
                     <div className="card-body">
                         <h5 className="card-title">{project.title}</h5>
                         <p className="card-text">{project.description}</p>
-                        <a href={project.githubLink} className="btn btn-primary" target="_blank" rel="noopener noreferrer">
+                        <a href={project.githubLink} className="btn btn-warning" target="_blank" rel="noopener noreferrer">
                             Github
                         </a>
                     </div>
